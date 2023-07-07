695c507bb9cbbdf0f26f6d363deed7115ea7f0a9
hataeyunn <gkxodbsa@gmail.com>
2023-07-07 13:28:08 +0900

    modifying ACK function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import simpy
 import random
+from collections import deque
 
 class Client:
     def __init__(self, env, network, acks, num_paths, packet_size=1500, initial_window_size=1):
@@ -11,15 +12,13 @@ class Client:
         self.window_sizes = [initial_window_size] * num_paths
 
     def send(self, path_index):
-        while True:
-            for _ in range(int(self.window_sizes[path_index])):
-                self.packet_number += 1
-                data = (self.packet_number, 'This is packet %d' % self.packet_number, path_index)
-                print(f"{self.env.now}s: {data[0]} packet using path {path_index} ")
-                self.network.send(data)
-            yield self.env.timeout(0.1)  # TODO 수정해야함 패킷을 전송한 후 1초 대기
+        for _ in range(int(self.window_sizes[path_index])):
+            self.packet_number += 1
+            data = (self.packet_number, 'This is packet %d' % self.packet_number, path_index)
+            print(f"At time {self.env.now}s: {data[0]} packet using path {path_index} ")
+            self.network.send(data)
 
-    def receive_ack(self):
+    def receive_ack_and_send(self):
         while True:
             ack = yield self.acks.get()
             # 혼잡창 크기 조정 알고리즘 구현
@@ -27,6 +26,13 @@ class Client:
                 self.window_sizes[ack[2]] /= 2  # 혼잡창을 절반으로 줄임
             else:
                 self.window_sizes[ack[2]] += 1  # 혼잡창을 증가시킴
+
+            # 혼잡 제어 창 크기만큼 패킷을 전송
+            for _ in range(int(self.window_sizes[ack[2]])):
+                self.packet_number += 1
+                data = (self.packet_number, 'This is packet %d' % self.packet_number, ack[2])
+                print(f"At time {self.env.now}s: {data[0]} packet using path {ack[2]} ")
+                self.network.send(data)
 
 
 class Network:
@@ -36,28 +42,33 @@ class Network:
         self.path_characteristics = path_characteristics
         self.num_paths = len(path_characteristics)
         self.incoming_queues = [simpy.Store(env) for _ in range(self.num_paths)]
+        self.packet_queues = [deque() for _ in range(self.num_paths)]
+        self.is_transmitting = [False for _ in range(self.num_paths)]
 
     def send(self, data):
         path_index = data[2]
-        self.incoming_queues[path_index].put(data)
-
-    def run(self):
-        for i in range(self.num_paths):
-            self.env.process(self._path_run(i))
+        self.packet_queues[path_index].append(data)
+        if not self.is_transmitting[path_index]:
+            self.env.process(self._path_run(path_index))
 
     def _path_run(self, path_index):
-        while True:
-            data = yield self.incoming_queues[path_index].get()
+        self.is_transmitting[path_index] = True
+        while self.packet_queues[path_index]:
+            data = self.packet_queues[path_index].popleft()
             bandwidth, latency, error_rate = self.path_characteristics[path_index]
             packet_size = 1500  # bytes
             transmission_time = packet_size / bandwidth  # Calculate transmission time
 
             if random.random() > error_rate:  # If not error
-                # 패킷이 도착하자마자 latency를 적용하고, 그 후에 패킷을 전송합니다.
-                yield self.env.timeout(latency)  # Apply latency immediately when packet arrives
+                # 패킷이 도착하자마자 latency/2를 적용하고, 그 후에 패킷을 전송합니다.
+                yield self.env.timeout(latency)  # Apply latency/2 immediately when packet arrives
                 yield self.env.timeout(transmission_time)  # Transmission delay
                 self.server.receive(data)
+        self.is_transmitting[path_index] = False
 
+    def run(self):
+        for i in range(self.num_paths):
+            self.env.process(self._path_run(i))
 
 class Server:
     def __init__(self, env, acks, path_characteristics):
@@ -65,21 +76,36 @@ class Server:
         self.received_data = []
         self.acks = acks
         self.path_characteristics = path_characteristics
+        self.ack_queues = [deque() for _ in range(len(path_characteristics))]
+        self.is_sending_ack = [False for _ in range(len(path_characteristics))]
+        self.packet_arrival_times = {}  # To record the arrival times of packets
 
     def receive(self, data):
         self.received_data.append(data)
-        if data[2]==2:
-            print(f"At time {self.env.now}, received packet number: {data[0]} on path {data[2]}")
+        if data[2] in [0, 1, 2]:
+            print(f"At time {self.env.now}s: received packet number: {data[0]} on path {data[2]}")
         ack = (data[0], 'ACK for packet %d' % data[0], data[2])
-        self.env.process(self.send_ack(ack))  # Process로 send_ack를 실행
+        self.ack_queues[data[2]].append(ack)
+        self.packet_arrival_times[data[0]] = self.env.now  # Record the packet arrival time
+        if not self.is_sending_ack[data[2]]:
+            self.env.process(self.send_ack(data[2]))
 
-    def send_ack(self, ack):
-        path_index = ack[2]
-        bandwidth, latency, error_rate = self.path_characteristics[path_index]
+    def send_ack(self, path_index):
+        self.is_sending_ack[path_index] = True
+        while self.ack_queues[path_index]:
+            ack = self.ack_queues[path_index].popleft()
+            bandwidth, latency, error_rate = self.path_characteristics[path_index]
+            packet_size = 1500  # bytes
 
-        if random.random() > error_rate:  # If not error
-            yield self.env.timeout(latency)  # Transmission delay
-            self.acks.put(ack)
+            if random.random() > error_rate:  # If not error
+                random.seed()
+                transmission_delay = random.uniform(0, bandwidth/packet_size)
+                transmission_delay = 0
+                yield self.env.timeout(max(0, self.packet_arrival_times[ack[0]] - self.env.now))  # Wait until the packet arrival time
+                yield self.env.timeout(transmission_delay + latency)  # Transmission delay
+                self.acks.put(ack)
+                print(f"At time {self.env.now}s: ACK of packet {ack[0]} arrived using path {ack[2]}")
+        self.is_sending_ack[path_index] = False
 
 
 env = simpy.Environment()
@@ -93,8 +119,8 @@ network = Network(env, server, path_characteristics)
 client = Client(env, network, acks, num_paths=num_paths)
 
 for i in range(num_paths):
-    env.process(client.send(i))
-env.process(client.receive_ack())
+    client.send(i)
+env.process(client.receive_ack_and_send())
 
 network.run()
 env.run(until=10)
